c74f851d7ff86ac8f5439334ce262eda601f9818
Dilli Bhandari <bhandaridilli1@gmail.com>
2021-07-25 11:23:36 +0545

    firebase initialization complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main()=>runApp(MyApp());
@@ -13,22 +14,39 @@ class MyApp extends StatelessWidget {
         primaryColor: Color(0xFFE7E9F5),
         accentColor: Color(0xFFE7E9F5),
       ),
-      home: HomePage(),
+      home: LandingPage(),
     );
   }
 }
-class HomePage extends StatelessWidget {
-  const HomePage({ Key? key }) : super(key: key);
+class LandingPage extends StatelessWidget {
+  final Future<FirebaseApp>_initialization = Firebase.initializeApp();
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Container(
-        color: Colors.red,
-        child: Center(
-          child: Text("hello world"),
-        ),
-      ),
+    return FutureBuilder(
+      future: _initialization,
+      builder: (context, snapshot){
+        if(snapshot.hasError){
+          return Scaffold(
+            body: Center(
+              child: Text("Error:${snapshot.error}"),
+            ),
+          );
+        }
+        //connection made to firebase
+        if(snapshot.connectionState==ConnectionState.done){
+          return Scaffold(
+            body: Container(child: Center(child: Text('Firebase App initialized'),),),
+          );
+        }
+        return Scaffold(
+          body: Center(
+            child: Text(
+              'Initialization app.....'
+            ),
+          ),
+        );
+      },
     );
   }
 }
